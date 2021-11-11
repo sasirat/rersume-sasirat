a89b1cdb6c294055524d0330f5d194b0b44cc499
sasirat <sasirat019@gmail.com>
2021-11-11 23:58:25 +0700

    Update interface

--- components/card.tsx
+++ components/card.tsx
@@ -2,37 +2,45 @@ import { useState, useEffect } from 'react'
 import axios from 'axios';
 import Skeleton from 'react-loading-skeleton'
 
+interface Pokemon {
+  name: string;
+  url: string;
+}
+
+interface PokemonResponse {
+  count: number;
+  next: string;
+  previous: null;
+  results: Pokemon[];
+}
+
 const ShowDetail = () => {
-  const [data, setData] = useState();
+  const [data, setData] = useState<PokemonResponse>();
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true);
-    axios.get('https://pokeapi.co/api/v2/pokemon?limit=104').then((response) => {
+    axios.get<PokemonResponse>('https://pokeapi.co/api/v2/pokemon?limit=104').then((response) => {
       setData(response.data);
       setLoading(false);
     });
   }, []);
 
 
-  // if (loading) {
-  //   return <p>Loading</p>;
-  // }
-
-  if (data === undefined) {
-    return <p>Data is undefined</p>;
+  if (loading) {
+    return <p>Loading...</p>
   }
+
   let regex = new RegExp('https:\/\/pokeapi\.co\/api\/v2\/pokemon\/(.*)\/')
-  // let result = regex.exec(`${data.results.url}`)
-  // console.log(result)
+
   return (
     <>
     <div className="text-center my-16">
       <p className="text-lg">POKEMON</p>
     </div>
-    <div className="m-auto text-center max-w-6xl">
+    <div className="m-auto text-center max-w-6xl mb-20">
       <div className="grid grid-cols-6 gap-6">
-        {data.results.map((item) =>  
+        {data?.results.map((item) =>  
           <div>
             <div className="flex justify-center items-center">
             {loading && (
@@ -45,8 +53,7 @@ const ShowDetail = () => {
               <p className="bg-green-200 rounded rounded-md">{loading ? <Skeleton /> : item.name}</p>
             </div>
           </div>
-        )
-        }
+        )}
       </div>
     </div>
     </>
