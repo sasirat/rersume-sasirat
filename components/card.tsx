381a5ac587e2002b2705993029fa6315650fb223
sasirat <sasirat019@gmail.com>
2021-11-12 01:35:19 +0700

    Update skeleton loading

--- components/card.tsx
+++ components/card.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, FC } from 'react'
 import axios from 'axios';
 import Skeleton from 'react-loading-skeleton'
 
@@ -14,6 +14,36 @@ interface PokemonResponse {
   results: Pokemon[];
 }
 
+const PokemonSkeletonLoading = () => {
+  return (
+    <Skeleton width={80} height={80} circle style={{ margin: '8px' }} />
+  )
+}
+
+const Pokemon: FC<Pokemon> = ({name, url}) => {
+  const [loading, setLoading] = useState(true);
+
+  let regex = new RegExp('https:\/\/pokeapi\.co\/api\/v2\/pokemon\/(.*)\/')
+  const id = regex.exec(`${url}`)![1];  
+
+  return (
+    <>
+    <div>
+      <div className="flex justify-center items-center">
+        {loading && <PokemonSkeletonLoading />}
+        <img
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} 
+          onLoad={() => setLoading(false)}
+        />
+      </div>
+      <div>
+        <p className="bg-green-200 rounded rounded-md">{name}</p>
+      </div>
+    </div>
+    </> 
+  ) 
+}
+
 const ShowDetail = () => {
   const [data, setData] = useState<PokemonResponse>();
   const [loading, setLoading] = useState(false);
@@ -27,12 +57,6 @@ const ShowDetail = () => {
   }, []);
 
 
-  if (loading) {
-    return <p>Loading...</p>
-  }
-
-  let regex = new RegExp('https:\/\/pokeapi\.co\/api\/v2\/pokemon\/(.*)\/')
-
   return (
     <>
     <div className="text-center my-16">
@@ -40,20 +64,17 @@ const ShowDetail = () => {
     </div>
     <div className="m-auto text-center max-w-6xl mb-20">
       <div className="grid grid-cols-6 gap-6">
-        {data?.results.map((item) =>  
-          <div>
-            <div className="flex justify-center items-center">
-            {loading && (
-                        <Skeleton
-                        />
-                    )}
-              <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${(regex.exec(`${item.url}`))[1]}.png`} style={{ display: loading ? 'none' : undefined }}/>
-            </div>
-            <div>
-              <p className="bg-green-200 rounded rounded-md">{loading ? <Skeleton /> : item.name}</p>
-            </div>
-          </div>
+        {loading && (
+          Array(60).fill(null).map((_, index) => (
+            <div key={index} className="flex justify-center items-center">
+              <div>
+              <PokemonSkeletonLoading />
+              <Skeleton style={{ width: '100%' }}/>
+              </div>
+            </div> 
+          ))
         )}
+        {data?.results.map(item => <Pokemon key={item.name} {...item}/>)}
       </div>
     </div>
     </>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import 'react-loading-skeleton/dist/skeleton.css'
+
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
